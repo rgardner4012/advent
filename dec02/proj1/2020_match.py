34fb824bf4fd2cef70b8320d126cb93c57f332fd
Batman <fruitfocker@gmail.com>
2020-12-02 23:42:54 -0500

    first try at match

--- dec02/proj1/2020_match.py
+++ dec02/proj1/2020_match.py
@@ -1,6 +1,10 @@
 #!/usr/local/bin/python3.6
-#import wget
-import requests
 
-expenseReport = requests.get('https://adventofcode.com/2020/day/1/input')
-print(expenseReport.text)
+with open('input.txt', 'r') as expenseReport:
+	for x in expenseReport.readlines():
+		print(x)
+		yang = 2020 - int(x)
+		print(yang)
+		if x == yang:
+			print('match!')
+
